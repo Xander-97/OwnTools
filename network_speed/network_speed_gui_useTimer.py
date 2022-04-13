7a0cdbf3ba2c060e8467fcd6a7e5cc32410a9dc7
Xander-97 <cleverfairy23@gmail.com>
2022-04-13 23:51:35 +0800

    modify little problems

--- network_speed/network_speed_gui_useTimer.py
+++ network_speed/network_speed_gui_useTimer.py
@@ -1,4 +1,4 @@
-#!usr/bin python3
+#!/usr/bin/env python3
 # coding:utf-8
 
 import os
@@ -9,15 +9,18 @@ from threading import Timer
 from PIL import Image, ImageTk
 
 PUBLIC_FONT = '"B612 Mono" 13 bold'
-# WIDTH = 230
-# HEIGHT = 120
-WIDTH = 260
-HEIGHT = 267
+WIDTH = 230
+HEIGHT = 120
+# WIDTH = 260
+# HEIGHT = 267
 BASIC_COLOR = 'gray'
-FONT_COLOR = "black"
+FONT_COLOR = "blue"
 ROOT = None
 CANVAS = None
 BACKGROUND = None
+
+# 初始化更新标志
+Flag = True
 
 
 def exit(e):
@@ -29,8 +32,17 @@ def on_resize(e):
     create_rectangle(CANVAS)
 
 
-def create_rectangle(_canvas):
-    _canvas.create_rectangle(0, 0, _canvas.winfo_width(), _canvas.winfo_height(), fill=BASIC_COLOR, outline=BASIC_COLOR)
+def set_content(sent, recv):
+    return ("\n\r上传: {}b/s".format("%.2f" % sent + 'K' if sent // 1000 == 0 else "%.2f" % (sent / 1000) + 'M'),
+            "\n\r下载: {}b/s".format("%.2f" % recv + 'K' if recv // 1000 == 0 else "%.2f" % (recv / 1000) + 'M'))
+
+
+def get_time(localtime):
+    return time.strftime("%Y-%m-%d %H:%M:%S", localtime)
+
+
+def create_rectangle(canvas):
+    canvas.create_rectangle(0, 0, canvas.winfo_width(), canvas.winfo_height(), fill=BASIC_COLOR, outline=BASIC_COLOR)
 
 
 def main():
@@ -40,8 +52,8 @@ def main():
     # root.overrideredirect(True)  # 无边框窗口
     canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
     canvas.grid(column=0, row=0)
-    BACKGROUND = ImageTk.PhotoImage(Image.open(os.path.dirname(__file__) + os.sep + 'pkq1.png'))
-    canvas.create_image(125, 125, anchor='center', image=BACKGROUND)
+    # BACKGROUND = ImageTk.PhotoImage(Image.open(os.path.dirname(__file__) + os.sep + 'pkq1.png'))
+    canvas.create_image(125, 125, anchor='center')  # image=BACKGROUND
     # create_rectangle(canvas)
     # 设置透明窗口
     root.wm_attributes('-transparentcolor', BASIC_COLOR)
@@ -54,18 +66,29 @@ class NetworkSpeedGui():
         self.canvas = _canvas
         self.width = _width
         self.height = _height
-        self.canvas.create_text(self.width / 2, self.height / 4, font=PUBLIC_FONT, tags='date', text='', fill=FONT_COLOR)  # tag带不带s都可以
-        self.canvas.create_text(self.width / 2, self.height / 3 + 10, font=PUBLIC_FONT, text='', tag='up', fill=FONT_COLOR)
-        self.canvas.create_text(self.width / 2, self.height / 2 + 20, font=PUBLIC_FONT, text='', tag='down', fill=FONT_COLOR)
+        self.canvas.create_text(self.width / 2, self.height / 4, font=PUBLIC_FONT, tags='date', text='', fill=FONT_COLOR, justify=tk.CENTER)  # tag带不带s都可以
+        self.canvas.create_text(self.width / 2, self.height / 3, font=PUBLIC_FONT, text='', tag='up', fill=FONT_COLOR, justify=tk.LEFT)
+        self.canvas.create_text(self.width / 2, self.height / 2 + 10, font=PUBLIC_FONT, text='', tag='down', fill=FONT_COLOR, justify=tk.LEFT)
         self.sent_before, self.recv_before = 0, 0
 
         # timer
-        self.timer = Timer(0.01, self.get_net_speed)
+        self.timer = Timer(0, self.get_net_speed)
         self.timer.start()
 
     def get_net_speed(self):
+        global Flag
+        
+        # 时间获取应该在此处,不然就隔着运行2秒 初始化数值 clock
+        self.canvas.itemconfigure('date', text=get_time(time.localtime()))
+        if Flag:
+            sent, recv = set_content(self.sent_before / 1024, self.recv_before / 1024)
+            self.canvas.itemconfigure('up', text=sent)
+            self.canvas.itemconfigure('down', text=recv)
+            Flag = False
+            
         self.sent_before = psutil.net_io_counters().bytes_sent  # 已发送的流量
         self.recv_before = psutil.net_io_counters().bytes_recv  # 已接收的流量
+
         self.timer.cancel()
         self.timer = Timer(1, self.update)
         self.timer.start()
@@ -75,21 +98,16 @@ class NetworkSpeedGui():
     def update(self):
         sent_now = psutil.net_io_counters().bytes_sent
         recv_now = psutil.net_io_counters().bytes_recv
-        sent = (sent_now - self.sent_before) / 1024  # 算出1秒后的差值
+        sent = (sent_now - self.sent_before) / 1024  # 计算出1秒后的差值
         recv = (recv_now - self.recv_before) / 1024
         print('update:', sent_now, recv_now)
-        data = [
-            time.strftime(" %Y-%m-%d %H:%M:%S ",
-                          time.localtime()),
-            "\n上传: {}b/s".format("%.2f" % sent + 'K' if sent // 1000 == 0 else "%.2f" % (sent / 1000) + 'M'),
-            "\n下载: {}b/s".format("%.2f" % recv + 'K' if recv // 1000 == 0 else "%.2f" % (recv / 1000) + 'M')
-        ]
+        data = [get_time(time.localtime()), *set_content(sent, recv)]
 
         self.canvas.itemconfigure('date', text=data[0])
         self.canvas.itemconfigure('up', text=data[1])
         self.canvas.itemconfigure('down', text=data[2])
         self.timer.cancel()
-        self.timer = Timer(1, self.get_net_speed)
+        self.timer = Timer(0, self.get_net_speed)
         self.timer.start()
 
 
